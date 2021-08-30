698ac761b443cb50bc0d55ce4631dd8c6f8a3c64
Alex Lehr <alexsmacbook@Alexs-MacBook-Air.local>
2021-08-29 19:04:12 -0500

    manipulated queries to show date ascending and no past dates

--- archive-event.php
+++ archive-event.php
@@ -8,7 +8,7 @@
             <p>See what is going on in our world.
             </p>
         </div>
-    </div>
+    </div>s
 </div>
 
 <div class="container container--narrow page-section">
@@ -16,20 +16,23 @@
     while (have_posts()) {
         the_post(); ?>
 
-<div class="event-summary">
-          <a class="event-summary__date t-center" href="#">
-            <span class="event-summary__month">Mar</span>
-            <span class="event-summary__day">25</span>
-          </a>
-          <div class="event-summary__content">
-            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink();?>"><?php the_title() ?></a></h5>
-            <p><?php
-                echo wp_trim_words(get_the_content(), 18)
-                ?><a href="<?php the_permalink();?>" class="nu gray">Learn more</a></p>
-          </div>
+        <div class="event-summary">
+            <a class="event-summary__date t-center" href="#">
+                <span class="event-summary__month"><?php
+                                                    $eventDate = new DateTime(get_field('event_date'));
+                                                    echo $eventDate->format('M')
+                                                    ?></span>
+                <span class="event-summary__day"><?php echo $eventDate->format('d') ?></span>
+            </a>
+            <div class="event-summary__content">
+                <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h5>
+                <p><?php
+                    echo wp_trim_words(get_the_content(), 18)
+                    ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+            </div>
         </div>
-        
-        <?php }
+
+    <?php }
 
     echo paginate_links();
     ?>
